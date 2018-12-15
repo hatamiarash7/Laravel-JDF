4fded5236cd1368b866d205f7262ad99b3077166
hatamiarash7 <hatamiarash7@gmail.com>
2018-12-16 01:42:09 +0330

    Create JDFServiceProvider.php

---
+++ src/JDFServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Hatamiarash7\JDF;
+
+use Illuminate\Support\ServiceProvider;
+
+class JDFServiceProvider extends ServiceProvider
+{
+	protected $defer = false;
+
+	public function boot()
+	{
+	}
+
+	public function register()
+	{
+	}
+}
